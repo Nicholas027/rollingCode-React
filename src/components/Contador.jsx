cfbb9a0c0c97796b130fb013756b69298decc798
Nicholas027 <nicholas0810152015@gmail.com>
2023-12-12 10:56:31 -0300

    contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,9 +1,13 @@
+import {useState} from 'react'
+
 const Contador = () => {
+    const [contador, setContador] = useState(0)
+
     return (
         <article className="text-center mt-4">
             <h3>Contador</h3>
-            <p className="fw-bold">10</p>
-            <button className="btn btn-primary">+1</button>
+            <p className="fw-bold">{contador}</p>
+            <button className="btn btn-primary" onClick={() => setContador(contador + 1)}>+1</button>
         </article>
     );
 };
