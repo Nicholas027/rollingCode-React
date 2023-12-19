e0aafeefefdce5c77d99bd8c6f46e83b406af4ea
Nicholas027 <nicholas0810152015@gmail.com>
2023-12-19 11:21:20 -0300

    componente formulario tarea

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import "./App.css"
+import FormularioTarea from "./components/FormularioTarea";
 
 function App() {
 
   return (
     <>
-      <Container className="my-4 app">
+      <Container className="my-4 mainPage">
         <h1 className="text-center text-light display-3">Lista de tareas</h1>
+        <FormularioTarea/>
       </Container>
       <Footer></Footer>
     </>
--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,9 +1,14 @@
-const FormularioTarea = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+import { Button, Form } from 'react-bootstrap';
+
+function FormularioTarea() {
+  return (
+    <Form>
+      <Form.Group className="mb-3 d-flex" controlId="formularioTarea">
+        <Form.Control type="text" placeholder="Ej: tarea 1" className='me-2'/>
+        <Button type='submit'>Agregar</Button>
+      </Form.Group>
+    </Form>
+  );
+}
 
 export default FormularioTarea;
