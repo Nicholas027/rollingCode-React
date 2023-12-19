aeb9aca750693b6f9fc89ff2ebead2977bf1d99d
Nicholas027 <nicholas0810152015@gmail.com>
2023-12-19 11:32:49 -0300

    Agregado itemTarea y listaTarea

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,13 +1,21 @@
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form } from "react-bootstrap";
+import ListaTareas from "./ListaTareas";
 
 function FormularioTarea() {
   return (
-    <Form>
-      <Form.Group className="mb-3 d-flex" controlId="formularioTarea">
-        <Form.Control type="text" placeholder="Ej: tarea 1" className='me-2'/>
-        <Button type='submit'>Agregar</Button>
-      </Form.Group>
-    </Form>
+    <section>
+      <Form>
+        <Form.Group className="mb-3 d-flex" controlId="formularioTarea">
+          <Form.Control
+            type="text"
+            placeholder="Ej: tarea 1"
+            className="me-2"
+          />
+          <Button type="submit">Agregar</Button>
+        </Form.Group>
+      </Form>
+      <ListaTareas />
+    </section>
   );
 }
 
